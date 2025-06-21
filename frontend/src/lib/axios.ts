91a4925883ae5d06bafc5fcb546af1b81dacdecf
ADITYA BASANTIA <adityabasantia729@gmail.com>
2025-06-22 01:14:05 +0530

    FundError

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000", // fallback
+  baseURL: import.meta.env. || "http://localhost:5000", // fallback
   withCredentials: true, // optional: if you need to send cookies
 });
 
